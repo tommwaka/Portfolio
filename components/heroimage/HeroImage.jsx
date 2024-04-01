c57b5da37f4ca00a00ea4835813de3792bf60b0c
alexkimeu <kimeu@pezesha.com>
2024-04-01 19:27:58 +0300

    Made styles changes

--- components/heroimage/HeroImage.jsx
+++ components/heroimage/HeroImage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 
-import hero from "../../public/assets/hero-bg.png";
+import hero from "../../public/assets/tom.jpeg";
 import figma from "../../public/assets/figma.png";
 import miro from "../../public/assets/miro.png";
 import xd from "../../public/assets/xd.png";
@@ -11,51 +11,51 @@ import zeplin from "../../public/assets/zeplin.png";
 import styles from "./HeroImage.module.css";
 
 const HeroImage = () => {
-	return (
-		<div
-			className={styles.container}
-			data-aos="zoom-in"
-			data-aos-duration="2200"
-		>
-			<div className={styles.image}>
-				<Image
-					src={hero}
-					alt="Thomas Mwaka"
-					data-aos="fade-up"
-					data-aos-duration="2400"
-					className={styles.heroImage}
-				/>
-				<div
-					className={styles.figma}
-					data-aos="fade-right"
-					data-aos-duration="2400"
-				>
-					<Image src={figma} alt="Figma" />
-				</div>
-				<div
-					className={styles.xd}
-					data-aos="fade-right"
-					data-aos-duration="2400"
-				>
-					<Image src={xd} alt="Adobe XD" />
-				</div>
-				<div
-					className={styles.miro}
-					data-aos="fade-right"
-					data-aos-duration="2400"
-				>
-					<Image src={miro} alt="Miro" />
-				</div>
-				<div
-					className={styles.zeplin}
-					data-aos="fade-left"
-					data-aos-duration="2400"
-				>
-					<Image src={zeplin} alt="Zeplin" />
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      className={styles.container}
+      data-aos="zoom-in"
+      data-aos-duration="2200"
+    >
+      <div className={styles.image}>
+        <Image
+          src={hero}
+          alt="Thomas Mwaka"
+          data-aos="fade-up"
+          data-aos-duration="2400"
+          className={styles.heroImage}
+        />
+        <div
+          className={styles.figma}
+          data-aos="fade-right"
+          data-aos-duration="2400"
+        >
+          <Image src={figma} alt="Figma" />
+        </div>
+        <div
+          className={styles.xd}
+          data-aos="fade-right"
+          data-aos-duration="2400"
+        >
+          <Image src={xd} alt="Adobe XD" />
+        </div>
+        <div
+          className={styles.miro}
+          data-aos="fade-right"
+          data-aos-duration="2400"
+        >
+          <Image src={miro} alt="Miro" />
+        </div>
+        <div
+          className={styles.zeplin}
+          data-aos="fade-left"
+          data-aos-duration="2400"
+        >
+          <Image src={zeplin} alt="Zeplin" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HeroImage;
--- data/testimonies.js
+++ data/testimonies.js
@@ -1,21 +1,21 @@
-import alex from "../public/assets/user.jpeg"
-import odipo from "../public/assets/odipo.jpeg"
+import alex from "../public/assets/user.jpeg";
+import odipo from "../public/assets/odipo.jpeg";
 
 export const testimonies = [
-	{
-		name: "Alex Kimeu",
-		role: "Software Engineer",
-		company: "Pezesha Africa",
-		description:
-			"Thomas is a highly talented individual in the design space. He has a keen eye for detail, a deep understanding of user needs, and a passion for creating beautiful and functional interfaces. As an independent contractor, I've had the privilege of working with him to deliver several products and I highly recommend him for any UI/UX design project confident that he will live up to the honor that comes with such an opportunity.",
-		image: alex,
-	},
-	{
-		name: "Stephen Odipo",
-		role: "Sr. Business Analyst",
-		company: "Deloitte Kenya",
-		description:
-			"Thomas is a highly skilled and experienced UX designer who consistently delivers user-centric designs that meet business objectives. His strong attention to detail, excellent communication skills, and passion for creating exceptional user experiences make him a valuable asset to any UX design team.",
-		image: odipo,
-	},
+  {
+    name: "Alex Kimeu",
+    role: "Software Engineer",
+    company: "Pezesha Africa",
+    description:
+      "Thomas is a highly talented individual in the design space. He has a keen eye for detail, a deep understanding of user needs & a passion for creating beautiful and functional interfaces. I've had the privilege of working with him to deliver several products & I highly recommend him for any UI/UX design project.",
+    image: alex,
+  },
+  {
+    name: "Stephen Odipo",
+    role: "Sr. Business Analyst",
+    company: "Deloitte Kenya",
+    description:
+      "Thomas is a highly skilled and experienced UX designer who consistently delivers user-centric designs that meet business objectives. His strong attention to detail, excellent communication skills, and passion for creating exceptional user experiences make him a valuable asset to any UX design team.",
+    image: odipo,
+  },
 ];
