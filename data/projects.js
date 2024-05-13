fa5c7ac65115cbc51f79a46409db72ef377ccd1a
tommwaka <mwakathomas7@gmail.com>
2024-05-14 01:00:07 +0300

    project Update

--- data/projects.js
+++ data/projects.js
@@ -2,6 +2,8 @@ import jumba from "../public/assets/Jumba.png"
 import agent from "../public/assets/Agent.png"
 import green from "../public/assets/Green.png"
 import wedco from "../public/assets/wedco.png"
+import hustler from "../public/assets/Hustler.png"
+import jada from "../public/assets/Jada.png"
 
 
 
@@ -17,8 +19,48 @@ export const projects = [
 	},
   
 	{
+		title: "Agent Term Loan App",
+		subtitle: "Project  2",
+		description:
+			"A gateway access to affordable loan products for our M-PESA agents with valid agent number.",
+		image: agent,
+	  caseStudyUrl: "https://www.behance.net/gallery/198453751/Agents-Mini-App",
+	  role: "UX Designer Lead",
+	},
+
+	{
+		title: "Hustler Connect",
+		subtitle: "Project 3",
+		description:
+		  "This app gives casual labourers a platform to be discovered by employers and get hired for short or long term jobs.",
+		image: hustler,
+		caseStudyUrl: "https://www.behance.net/gallery/198501409/Hustler-Connect",
+		role: "UX Designer Lead",
+	  },
+
+	  {
+		title: "Green Financing App",
+		subtitle: "Project 4",
+		description:
+			"A product that offers access zero carbon emission devices with carbon credit awards. This is to promote green financing initiatives",
+		image: green,
+		caseStudyUrl: "https://www.behance.net/gallery/198453049/Green-Financing-App",
+	    role: "UX Designer Lead",
+	},
+
+	  {
+		title: "Jada Base",
+		subtitle: "Project 5",
+		description:
+		  "This app is for a water supply company where employed technicians i.e plumbers get onboarded, assinged and track their tasks.",
+		image: jada,
+		caseStudyUrl: "https://www.behance.net/gallery/198480637/Jada-Base-App",
+		role: "UX Designer Lead",
+	  },
+
+	{
 	  title: "WEDCO Student Management System",
-	  subtitle: "Project 2",
+	  subtitle: "Project 6",
 	  description:
 		"This is a system designed and developed to enable the organization enroll students, manage students and provide coaching resources to the students.",
 	  image: wedco,
@@ -26,23 +68,5 @@ export const projects = [
 	  role: "UX Designer Lead",
 	},
   
-	{
-		title: "Agent Term Loan App",
-		subtitle: "Project  3",
-		description:
-			"A gateway access to affordable loan products for our M-PESA agents with valid agent number.",
-		image: agent,
-	  caseStudyUrl: "#",
-	  role: "UX Designer Lead",
-	},
-  
-	{
-		title: "Green Financing App",
-		subtitle: "Project 4",
-		description:
-			"A product to offer access zero carbon emission devices with carbon credit awards. This is to promote green financing initiatives",
-		image: green,
-		caseStudyUrl: "#",
-	    role: "UX Designer Lead",
-	},
+	
   ];
